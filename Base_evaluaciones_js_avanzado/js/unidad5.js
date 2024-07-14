b498013706fbefc6b68beb739cc134ce68c0cd10
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2024-07-14 19:50:55 -0300

    Update unidad5.js
    en parte

--- Base_evaluaciones_js_avanzado/js/unidad5.js
+++ Base_evaluaciones_js_avanzado/js/unidad5.js
@@ -1,26 +1,39 @@
 function ejecutar() {
+  let ejecutar = new Promise((resolver, reject) => {
     animarCuadrado(300, 100, 100, div => {
       div.classList.add('mensaje');
       div.append("Unidad 5");
     });
-  }
+  })
 
-  function animarCuadrado(cx, cy, radius, callback) {
-    let div = document.createElement('div');
-    div.style.width = 0;
-    div.style.height = 0;
-    div.style.left = cx + 'px';
-    div.style.top = cy + 'px';
-    div.className = 'cuadrado';
-    document.body.append(div);
+  ejecutar .then(resultado => {
+    console.log(resultado);
+    // La operación se completó con éxito, resultado contiene el valor resuelto console.log(resultado);
+    }) .catch(error => {
+    // La operación falló, error contiene el motivo del rechazo
+    console.error(error);
+    });
+}
 
-    setTimeout(() => {
-      div.style.width = radius * 2 + 'px';
-      div.style.height = radius * 2 + 'px';
+function animarCuadrado(cx, cy, radius, callback) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.className = 'cuadrado';
+  document.body.append(div);
 
-      div.addEventListener('transitionend', function handler() {
-        div.removeEventListener('transitionend', handler);
-        callback(div);
-      });
-    }, 0);
-  }
+  setTimeout(() => {
+    div.style.width = radius * 2 + 'px';
+    div.style.height = radius * 2 + 'px';
+
+    div.addEventListener('transitionend', function handler() {
+      div.removeEventListener('transitionend', handler);
+      callback(div);
+    });
+  }, 0);
+}
+
+
+
