c5a96b6fb6ab13ded27f1041cf6aaddc10c23bb7
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2024-07-21 15:22:30 -0300

    unidad 5 completa

--- Base_evaluaciones_js_avanzado/js/unidad5.js
+++ Base_evaluaciones_js_avanzado/js/unidad5.js
@@ -1,39 +1,36 @@
-function ejecutar() {
-  let ejecutar = new Promise((resolver, reject) => {
-    animarCuadrado(300, 100, 100, div => {
-      div.classList.add('mensaje');
-      div.append("Unidad 5");
-    });
+function animarCuadrado(cx, cy, radius) {
+  return new Promise((resolver, rechazar) => {
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'cuadrado';
+    document.body.append(div);
+
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+
+      div.addEventListener('transitionend', function handler() {
+        div.removeEventListener('transitionend', handler);
+        resolver(div);
+      });
+    }, 0);
+
+    if (!div) {
+      rechazar('Error al crear el div');
+    }
+  });
+}
+
+function Ejecutar(){
+  animarCuadrado(300, 100, 100)
+  .then(div => {
+    div.classList.add('mensaje');
+    div.append("Unidad 5");
   })
-
-  ejecutar .then(resultado => {
-    console.log(resultado);
-    // La operación se completó con éxito, resultado contiene el valor resuelto console.log(resultado);
-    }) .catch(error => {
-    // La operación falló, error contiene el motivo del rechazo
-    console.error(error);
-    });
+  .catch(error => {
+    console.error('Error:', error);
+  });
 }
-
-function animarCuadrado(cx, cy, radius, callback) {
-  let div = document.createElement('div');
-  div.style.width = 0;
-  div.style.height = 0;
-  div.style.left = cx + 'px';
-  div.style.top = cy + 'px';
-  div.className = 'cuadrado';
-  document.body.append(div);
-
-  setTimeout(() => {
-    div.style.width = radius * 2 + 'px';
-    div.style.height = radius * 2 + 'px';
-
-    div.addEventListener('transitionend', function handler() {
-      div.removeEventListener('transitionend', handler);
-      callback(div);
-    });
-  }, 0);
-}
-
-
-
