bd57d6aee798be7efd973021eb78569e0e9777b6
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2024-07-30 10:52:36 -0300

    unidad 6

---
+++ Base_evaluaciones_js_avanzado/js/unidad6.js
@@ -0,0 +1,26 @@
+class Productos{
+
+    constructor(codigo, categoria, nombre, precio){
+        this.codigo = codigo;
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+
+    ImprimeDatos(){
+        let contenido = `<p>Código: ${this.codigo} </p>`;
+        contenido += `<p>Categoria: ${this.categoria} </p>`;
+        contenido += `<p>Nombre: ${this.nombre} </p>`;
+        contenido += `<p>Precio: ${this.precio} </p>`;
+
+        document.getElementById('infoProducto').innerHTML = contenido;
+        console.log(contenido);
+    }
+
+
+}
+
+
+var producto = new Productos(1, 'Muebles', 'Escritorio', 250000);
+producto.ImprimeDatos();
